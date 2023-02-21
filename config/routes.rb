c6a19d5a274bb4d1f1c442cf66036333a73e583f
Harshit Sultaniya <harshitsultaniya@preciousinfosystem.com>
2023-02-21 13:18:09 +0530

    SignUp Page Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'user/create'
+  get 'user/new'
+  get 'user/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/signup' => 'user#new'
+  post '/users' => 'user#create'
+  
+  get '/' => 'user#index'
 end
