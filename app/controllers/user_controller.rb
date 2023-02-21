cea1421b7e1d6f4417f49b45d6b3b0b372165f54
Harshit Sultaniya <harshitsultaniya@preciousinfosystem.com>
2023-02-21 13:17:54 +0530

    User Controller Added

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,22 @@
+class UserController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      flash[:notice]="Signup successful"
+    else
+      flash[:notice]="Please try again"
+      redirect_to '/signup'
+    end
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:user_name, :password)
+    end
+end
